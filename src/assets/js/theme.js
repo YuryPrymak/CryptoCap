d2583e1b1fc9ee5d4f588a782c81a0f7b63385fd
Yury Prymak <YuryPrymak@gmail.com>
2022-01-24 11:56:19 +0200

    Add light theme

---
+++ src/assets/js/theme.js
@@ -0,0 +1,44 @@
+export default (() => {
+  let btnThemeToggle = null;
+  const storageKey = 'theme-preference';
+
+  const getColorPreference = () => {
+    if (localStorage.getItem(storageKey)) {
+      return localStorage.getItem(storageKey);
+    } else {
+      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    }
+  };
+
+  const theme = {
+    value: getColorPreference(),
+  };
+
+  const reflectPreference = () => {
+    document.firstElementChild.setAttribute('data-theme', theme.value);
+  };
+
+  const setPreference = () => {
+    localStorage.setItem(storageKey, theme.value);
+    reflectPreference();
+  };
+
+  const toggleTheme = () => {
+    theme.value = theme.value === 'light' ? 'dark' : 'light';
+
+    setPreference();
+  };
+
+  reflectPreference();
+
+  window.addEventListener('load', () => {
+    btnThemeToggle = document.querySelector('[data-btn-theme-toggle]');
+    reflectPreference();
+    btnThemeToggle.addEventListener('click', toggleTheme);
+  });
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ({ matches: isDark }) => {
+    theme.value = isDark ? 'dark' : 'light';
+    setPreference();
+  });
+})();
